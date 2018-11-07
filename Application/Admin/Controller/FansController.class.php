aa5e22773c4c7b146926889caf75b96747332e7c
jicy <510434563@qq.com>
2018-11-07 08:45:31 +0800

    粉丝列表

---
+++ Application/Admin/Controller/FansController.class.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright (c) 山东六牛网络科技有限公司 https://www.liuniukeji.com
+ *
+ * @Description    粉丝管理
+ * @Author         
+ * @Date           2018/10/25
+ * @CreateBy       PhpStorm
+ */
+namespace Admin\Controller;
+class FansController extends CommonController {
+
+    public function listFans() {
+        $user_id = I('user_id', 0, 'intval');
+        $where['f.status'] = array('eq',1);
+        $where['f.user_id'] = array('eq', $user_id);
+        $data = D('Admin/Fans')->getFansList($where);
+
+        $this->assign('list', $data['info']);
+        $this->assign('page', $data['page']);
+        $this->display();
+    }
+
+    public function recycle() {
+        $this->_recycle('vip_level','id');
+    }
+
+    public function del() {
+        $this->_del('vip_level', 'id');
+    }
+}
+        
---
+++ Application/Admin/Model/FansModel.class.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright (c) 山东六牛网络科技有限公司 https://www.liuniukeji.com
+ *
+ * @Description     粉丝管理
+ * @Author          jicy 510434563
+ * @Date           2018/10/25
+ * @CreateBy       PhpStorm
+ */
+namespace Admin\Model;
+
+use Think\Model;
+
+class FansModel extends Model {
+
+
+    /**
+     * 粉丝列表
+     * @param array $where
+     * @param string $field
+     * @param string $order
+     * @return array
+     */
+    public function getFansList($where = [], $field = '', $order = 'f.add_time desc') {
+        if ($field == '') {
+            $field = 'f.*,u.nick_name';
+        }
+        $count = $this->alias('f')->where($where)->count();
+        $page = get_page($count);
+        $list = $this->alias('f')
+            ->join('__USER__ u on f.fans_user_id = u.user_id')
+            ->field($field)
+            ->where($where)
+            ->limit($page['limit'])
+            ->order($order)
+            ->select();
+
+        return array(
+            'info'=>$list,
+            'page'=>$page['page']
+        );
+
+    }
+
+}
