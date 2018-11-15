70c061f7d15e121fb237763800560ba7de0d3dec
wangzhenyu <byzhenyu@qq.com>
2018-11-15 17:15:33 +0800

    微信扫码

--- Application/Mobile/Controller/UserController.class.php
+++ Application/Mobile/Controller/UserController.class.php
@@ -22,6 +22,8 @@ class UserController extends CommonController {
     * @return mixed
     */
     public function personalCenter(){
+           echo UID;
+           die;
           $user_id = UID;
           $where['u.user_id'] = $user_id;
           $field = 'u.head_pic, u.nick_name, u.total_money,u.bonus_money,s.shop_type, u.task_suc_money,u.user_id, s.shop_accounts,s.take_task';
