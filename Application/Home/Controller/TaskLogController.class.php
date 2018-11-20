9cbb979891d6e914667e95bee2c05a51c96c1324
zhanglx <527459901@qq.com>
2018-11-20 15:09:19 +0800

    en

--- Application/Home/Controller/TaskLogController.class.php
+++ Application/Home/Controller/TaskLogController.class.php
@@ -15,8 +15,37 @@ use Common\Controller\UserCommonController;
 
 class TaskLogController extends UserCommonController{
 
-       public function getTaskLog()
-       {
-       	   $this->display();
-       }
+    /**
+     * @desc 获取接单信息
+     * @param  $user_id
+     * @return mixed
+     */
+    public function getTaskLog()
+    {
+        /*where 接任务条件查找  0待提交 1审核中 2不合格 3已完成 default全部 */
+        $type = I('type', 5, 'intval');
+        switch ($type) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+                $where['l.valid_status'] = array('eq', $type);
+                break;
+            default:
+                $where['l.valid_status'] = array('neq', 4);
+                break;
+        }
+        $where['l.user_id'] = UID;
+        $field = 'l.id, l.task_id, l.task_name,l.valid_time, l.valid_status, t.price, c.category_name, c.category_img';
+        $taskLogModel = D('Home/TaskLog');
+        $taskLogInfo = $taskLogModel->getTaskLog($where,$field);
+
+        if (IS_POST) {
+            $this->ajaxReturn(V(1, '接单记录', $taskLogInfo['list']));
+
+        }
+        $this->assign('type', $type);
+        $this->assign('taskLogInfo', $taskLogInfo);
+        $this->display();
+    }
 }
