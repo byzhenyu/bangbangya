229b0fc9f5df30be06cbae23540b8fa09dd95f56
zhanglx <527459901@qq.com>
2018-11-20 17:49:41 +0800

    en

--- Application/Home/Controller/TaskLogController.class.php
+++ Application/Home/Controller/TaskLogController.class.php
@@ -68,4 +68,73 @@ class TaskLogController extends UserCommonController{
             $this->ajaxReturn(V(0, $this->TaskLogModel->getDbError()));
         }
     }
+
+    /**
+     * @desc  任务不合格详情
+     * @param  tasklog_id
+     * @return mixed
+     */
+    public function taskLogFail(){
+        $taskLog_id = I('id', 0, 'intval');
+        $taskLogInfo = $this->TaskLogModel->field('id, user_id, task_id, valid_pic')->where('id = '.$taskLog_id)->find();
+        $chatModel = D('Home/Chat');
+        $taskLogInfo['userChat']  = $chatModel ->field('content')->where('user_id  = '.$taskLogInfo['user_id'].'  and task_log_id =  '.$taskLogInfo['id'])->select();
+        $taskLogInfo['taskChat']  = $chatModel ->field('content')->where('task_user_id = '.$taskLogInfo['user_id'].'  and task_log_id =  '.$taskLogInfo['id'])->select();
+        if(strpos($taskLogInfo['valid_pic'], ',')  !== false){
+            $taskLogInfo['valid_pic']   =   explode(',',$taskLogInfo['valid_pic']);
+        } else {
+            $taskLogInfo['valid_pic']   =    array($taskLogInfo['valid_pic']);
+        }
+        $this->assign('taskLogInfo',$taskLogInfo);
+        $this->display();
+    }
+    /**
+     * @desc  放弃任务
+     * @param tasklog_id
+     * @return mixed
+     */
+    public function giveUpTask(){
+        $taskLog_id = I('id', 0, 'intval');
+        $task_id = I('task_id', 0, 'intval');
+        $taskModel = D('Home/Task');
+        M()->startTrans();
+        $taskLogRes = $this->TaskLogModel->where('id = '.$taskLog_id)->save(array('valid_status' => 4));
+        /*放弃任务 释放单子 数*/
+        $taskRes = $taskModel->where('id = '.$task_id)->setInc('task_num');
+        if($taskLogRes && $taskRes){
+            M()->commit();
+            $this->ajaxReturn(V(1, '成功'));
+        }else{
+            M()->rollback();
+            $this->ajaxReturn(V(0, '失败'));
+        }
+    }
+
+    /**
+     *  任务详情
+     */
+    public function taskLogDetail() {
+        $id = I('id', 0, 'intval');
+        $info = D('Home/TaskLog')->getTaskLogDetail(array('l.id'=>$id));
+
+        $this->assign('taskDetail', $info);
+        $this->display();
+    }
+
+    /**
+     * 重做
+     * 返回新logid
+     */
+    public function reDoLog() {
+        $log_id = I('log_id', 0 , 'intval');
+
+        $new_id = D('Home/TaskLog')->reDoTaskLog($log_id);
+
+        if ($new_id === false) {
+            $this->ajaxReturn(V(0, '操作失败'));
+        } else {
+            $this->ajaxReturn(V(1, '操作成功',$new_id));
+        }
+
+    }
 }
