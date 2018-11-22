5a625c1b0af93887f7a2f10c7e03f8e05a99d6c8
wangzhenyu <byzhenyu@qq.com>
2018-11-22 19:01:16 +0800

    alipay

--- Application/Payment/Controller/AlipayController.class.php
+++ Application/Payment/Controller/AlipayController.class.php
@@ -8,16 +8,24 @@ use Common\Controller\CommonController;
 
 class AlipayController extends CommonController {
 
-    // alipay 定单支付
+    // alipay 定单支付  type  0 余额充值 1 保证金充值
     public function alipay(){
+        $type =  I('type', 0, 'intval');
         $data['user_id'] = UID;
         $data['recharge_money'] = I('recharge_money',0 , 'intval');
+        $order_sn = makeOrderSn($data['user_id']);
+        if($type == 1){
+            $data['order_sn'] = 'T'.$order_sn;
+        }else{
+            $data['order_sn'] = 'B'.$order_sn;
+        }
         $data['order_sn'] = makeOrderSn($data['user_id']);
         M('recharge')->add($data);
         $data['body'] = '网页充值';
         $data['subject'] = '网页充值';
         $data['out_trade_no'] =  $data['order_sn'];
         $data['total_amount'] = '0.01';
+        header("Content-type: text/html; charset=utf-8");
         require_once("./Plugins/AliPay/AliPay.php");
         $alipay = new \AliPay();
         echo '页面跳转中, 请稍后...';
@@ -31,19 +39,18 @@ class AlipayController extends CommonController {
         //验证是否是支付宝发送
         $flag = $alipay->AliPayNotifyCheck();
         if ($flag) {
-            p($_POST);
             if ($_POST['trade_status'] == 'TRADE_FINISHED' || $_POST['trade_status'] == 'TRADE_SUCCESS') {
-//                $out_trade_no = trim($_POST['out_trade_no']); //商户订单号
-//                $total_amount = trim($_POST['total_amount']); //支付的金额
-//                $trade_no = trim($_POST['trade_no']); //商户订单号
-//                //成功后的业务逻辑处理
-//                $result = D('Common/PayReturn')->paySuccess($out_trade_no, $total_amount, $trade_no, 1);
-//                if ($result['status'] == 1) {
-//                    echo "success"; //  告诉支付宝支付成功 请不要修改或删除
-//                    die;
-//                } else {
-//                    LL($result);
-//                }
+                $out_trade_no = trim($_POST['out_trade_no']); //商户订单号
+                $total_amount = trim($_POST['total_amount']); //支付的金额
+                $trade_no = trim($_POST['trade_no']); //商户订单号
+                //成功后的业务逻辑处理
+                $result = D('Common/PayReturn')->paySuccess($out_trade_no, $total_amount, $trade_no, 1);
+                if ($result['status'] == 1) {
+                    echo "success"; //  告诉支付宝支付成功 请不要修改或删除
+                    die;
+                } else {
+                    LL($result);
+                }
             }
         }
         echo "fail"; //验证失败
