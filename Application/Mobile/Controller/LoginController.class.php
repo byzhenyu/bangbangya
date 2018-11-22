7623e86d51e425a263b56c5c931a65e3c7d52ce7
jicy <510434563@qq.com>
2018-11-22 17:14:49 +0800

    jPush

--- Application/Mobile/Controller/LoginController.class.php
+++ Application/Mobile/Controller/LoginController.class.php
@@ -62,8 +62,7 @@ class LoginController extends CommonController {
                 $shopDate = array(
                     'user_id' => $userid,
                     'shop_name' => $weiChatData['nickname'] . '的店铺',
-                    'shop_img' => $weiChatData['headimgurl'],
-                    'add_time' => NOW_TIME
+                    'shop_img' => $weiChatData['headimgurl']
                 );
                 D('Home/Shop')->add($shopDate);
                 $userInfo = $userModel->doLogin($weiChatData['openid']);
@@ -81,5 +80,58 @@ class LoginController extends CommonController {
         session('user_auth',null);
         define('UID', null);
         $this->ajaxReturn(V(1, '您退出了登录'));
+    }
+
+    //微信登陆
+    public function thirdLogin() {
+        $open_id = I('id', '');
+        $nick_name = I('name', '');
+        $head_pic = I('image', '');
+
+        if (!$open_id){
+            $this->ajaxReturn(V(0, '参数有误'));
+        }
+
+        $where['open_id'] = $open_id;
+        $where['status'] = array('eq', 1);
+
+        $userModel = D('Home/User');
+        $select = 'u.user_id,u.head_pic,u.nick_name,u.invitation_code,u.open_id,s.shop_accounts,s.top_time,s.shop_type,s.partner_time,s.take_task,s.task_count,s.task_num,s.vol,s.appeal_num,s.be_appeal_num,s.complain_num,s.be_complain_num';
+        $user = $userModel->getUserInfo(array('open_id'=>$open_id), $select);
+        unset($where);
+        if (!empty($user)) {
+            $map['nick_name'] = $nick_name;
+            $map['head_pic'] = $head_pic;
+            $map['register_time'] = time();
+
+            $userid = $userModel->add($map);
+            if ($userid) {
+                $invitation_code = $userModel->createCode($userid);
+                $userModel->where(array('user_id'=>$userid))->setField('invitation_code',$invitation_code);
+                if ($userid) {
+                    $shopDate = array(
+                        'user_id' => $userid,
+                        'shop_name' => $nick_name . '的店铺',
+                        'shop_img' => $head_pic,
+                        'add_time' => NOW_TIME
+                    );
+                    D('Home/Shop')->add($shopDate);
+                    $userInfo = $userModel->doLogin($open_id);
+                    session('user_auth', $userInfo['data']);
+                    define(UID, session('user_auth')['user_id']);
+                }
+
+                $this->ajaxReturn(V(1, '登录成功', $user['user_id']));
+            } else {
+                $this->ajaxReturn(V(0, '登录失败'));
+            }
+
+        } else {
+            session('user_auth', $user);
+            define(UID, session('user_auth')['user_id']);
+
+            $this->ajaxReturn(V(1, '登录成功', $user['user_id']));
+        }
+
     }
 }
