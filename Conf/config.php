f12cafa43e3d994622db69394674df9ec50ed793
wangzhenyu <byzhenyu@qq.com>
2018-11-22 19:07:42 +0800

    user_headPic changge;

--- Conf/config.php
+++ Conf/config.php
@@ -105,7 +105,7 @@
             /*支付宝回调地址*/
             'notifyUrl'    => 'http://bby.host5.liuniukeji.net/index.php/Payment/Alipay/alipayNotify',
             /*用于web支付返回地址*/
-            'returnUrl'    => 'http://bby.host5.liuniukeji.net/index.php/Payment/Alipay/AlipayReturn',
+            'returnUrl'    => 'http://bby.host5.liuniukeji.net/index.php/Payment/Alipay/alipayNotify',
         ),
 
 
