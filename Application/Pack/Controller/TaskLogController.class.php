eb81e8db69f5da05d2444cc0fca773f39d46bd1f
wangzhenyu <byzhenyu@qq.com>
2018-11-28 20:34:47 +0800

    pack task no pass detail

--- Application/Pack/Controller/TaskLogController.class.php
+++ Application/Pack/Controller/TaskLogController.class.php
@@ -241,13 +241,14 @@ class TaskLogController extends CommonController {
              $tasklogInfo = $this->TaskLogModel->where('id = '.$data['id'])->find();
              M()->startTrans();
              $tasklogRes = $this->TaskLogModel->where('id = '.$data['id'])->save($data);
+             $taskRes = $this->TaskModel->where(array('id' =>$tasklogInfo['task_id']))->setInc('task_num');
              $ChatModel = D('Home/Chat');
              $CharData['user_id'] = $tasklogInfo['user_id'];
              $CharData['task_user_id'] = UID;
              $CharData['task_log_id'] = $tasklogInfo['id'];
              $CharData['content'] = $data['valid_text'];
              $chatRes = $ChatModel->add($CharData);
-             if($tasklogRes  && $chatRes ){
+             if($tasklogRes  && $chatRes && $taskRes){
                  D('Common/Push')->push('任务处理通知',$tasklogInfo['user_id'],'亲，您的做的任务审核未通过!','任务名称:'.$tasklogInfo['task_name'],'通知类型：失败',$data['valid_text']);
                  M()->commit();
                  $this->ajaxReturn(V(1, '完成'));
