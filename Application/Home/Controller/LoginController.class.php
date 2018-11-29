03f836601cedaeb0543d6e48619cd978d53da668
wangzhenyu <byzhenyu@qq.com>
2018-11-29 19:46:44 +0800

    登录

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -31,8 +31,7 @@ class LoginController extends CommonController{
         $userInfo = $userModel->doLogin($weiChatData['openid']);
         if ($userInfo['data']['status'] == 1) { //登录成功
             if ($userInfo['data']['disabled'] == 0) {
-                $this->redirect('Home/index/index/login/2');
-                die;
+                V(3, '您的账号已被停用');
             }
             /* 存入session */
             session('user_auth', $userInfo['data']);
