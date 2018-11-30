4cc5b44541791b744c91c16d99def2038e3352f6
jicy <510434563@qq.com>
2018-11-30 15:33:00 +0800

    jPush

--- Application/Home/Controller/TaskLogController.class.php
+++ Application/Home/Controller/TaskLogController.class.php
@@ -141,7 +141,7 @@ class TaskLogController extends UserCommonController{
     public function taskLogDetail() {
         $id = I('id', 0, 'intval');
         $info = D('Home/TaskLog')->getTaskLogDetail(array('l.id'=>$id));
-//        p($info);
+        //p($info);
         $this->assign('taskDetail', $info);
         $this->display();
     }
@@ -261,7 +261,7 @@ class TaskLogController extends UserCommonController{
             $ShopModel->where('user_id = '.UID)->setInc('vol');
             $taskUser = array(
                 'task_suc_money' => array('exp','task_suc_money + '.$tasklogInfo['task_price']),
-                'total_money' => array('exp','total_money + '.$tasklogInfo['task_price'])
+                'total_money' => array('exp', 'total_money +'.$tasklogInfo['task_price'] ),
             );
             $userModel->where('user_id = '.$tasklogInfo['user_id'])->save($taskUser);
             account_log($tasklogInfo['user_id'], $tasklogInfo['task_price'], 4,'完成任务', $tasklog_id);
