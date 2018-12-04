9417469f013c88729c52ce56532a17cd2655e283
wangzhenyu <byzhenyu@qq.com>
2018-12-04 14:39:14 +0800

    1

--- Application/Pack/Controller/LoginController.class.php
+++ Application/Pack/Controller/LoginController.class.php
@@ -31,7 +31,7 @@ class LoginController extends CommonController {
     {
           /*测试打开*/
         $userModel = D('Home/User');
-        $userInfo = $userModel->doLogin('o_l6z0iWc1b2kklfSu7j3BiqQ2zw');
+        $userInfo = $userModel->doLogin('o_l6z0pSLavAEyvks0DqelxcmF3o');
         session('user_auth', $userInfo['data']);
         define(UID, session('user_auth')['user_id']);
         $this->redirect('Pack/User/Invitation/user_id/'.UID);
