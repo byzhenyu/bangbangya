7ef88ed349e532af7da27deb33255e506056312f
wangzhenyu <byzhenyu@qq.com>
2018-12-06 08:31:03 +0800

    1

--- Application/Payment/Controller/AlipayController.class.php
+++ Application/Payment/Controller/AlipayController.class.php
@@ -26,7 +26,7 @@ class AlipayController extends CommonController {
         $data['body'] = C('APP_NAME').'网页充值';
         $data['subject'] = C('APP_NAME').'网页充值';
         $data['out_trade_no'] =  $data['order_sn'];
-        $data['total_amount'] = '0.01';
+        $data['total_amount'] = $data['recharge_money'];
         header("Content-type: text/html; charset=utf-8");
         require_once("./Plugins/AliPay/AliPay.php");
         $aliData = C('AliPay');
@@ -55,7 +55,7 @@ class AlipayController extends CommonController {
         $data['body'] = C('APP_NAME').'H5充值';
         $data['subject'] = C('APP_NAME').'H5充值';
         $data['out_trade_no'] =  $data['order_sn'];
-        $data['total_amount'] = '0.01';
+        $data['total_amount'] =  $recharge_money;
         $aliData = C('AliPay');
         $aliData['returnUrl'] =  'http://bby.host5.liuniukeji.net/index.php/Mobile/User/personalCenter';
         require_once("./Plugins/AliPay/AliPay.php");
@@ -83,7 +83,7 @@ class AlipayController extends CommonController {
         $data['body'] = C('APP_NAME').'充值';
         $data['subject'] = C('APP_NAME').'充值';
         $data['out_trade_no'] =  $data['order_sn'];
-        $data['total_amount'] = '0.01';
+        $data['total_amount'] = $recharge_money;
         require_once("./Plugins/AliPay/AliPay.php");
         $alipay = new \AliPay();
         $result = $alipay->AliPayApp($data);
